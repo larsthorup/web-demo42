2785fbb5ecefc7641a7eb01120c971e4e17a515a
Lars Thorup <lars@zealake.com>
2023-02-17 10:25:45 +0100

    music album search

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { FormEvent, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function AlbumPicker() {
+  const [albums, setAlbums] = useState<string[]>([]);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      artist: { value: string };
+    };
+    const artist = encodeURIComponent(target.artist.value);
+    const url = `https://musicbrainz.org/ws/2/release?fmt=json&query=artist:${artist}`;
+    console.log({ url, artist });
+    const response = await fetch(url);
+    const mbResult = (await response.json()) as {
+      releases: { title: string }[];
+    };
+    const { releases } = mbResult;
+    setAlbums(releases.map(({ title }) => title));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Artist name:
+        <input name="artist" />
+      </label>
+      <button type="submit">Search</button>
+      <p>Albums:</p>
+      <ol>
+        {albums.map((album, index) => (
+          <li key={index}>{album}</li>
+        ))}
+      </ol>
+    </form>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <AlbumPicker />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
