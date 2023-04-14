10695af5d0c8549c4e1cdc0e5960ffd2ec0bf5bb
Lars Thorup <lars@zealake.com>
2023-04-14 16:11:32 +0200

    verify session token signature

--- validateUser.js
+++ validateUser.js
@@ -1,11 +1,27 @@
 import DescopeClient from "@descope/node-sdk";
+import { importJWK } from "jose";
+import jwt from "jsonwebtoken";
+import { promisify } from "node:util";
 
 const sessionToken = process.argv[2];
-
+const projectId = "P2OPANdSmtackRAJlrRKFxFcoriw";
 try {
   const descopeClient = DescopeClient({
-    projectId: "P2OPANdSmtackRAJlrRKFxFcoriw",
+    projectId,
   });
+
+  // fetch public key
+  const jwks = await (
+    await fetch(`https://api.descope.com/v2/keys/${projectId}`)
+  ).json();
+  const publicKey = await importJWK(jwks.keys[0]);
+
+  // verify session token signature
+  await promisify(jwt.verify)(sessionToken, publicKey, {
+    algorithms: ["RS256"],
+  });
+
+  // validate session token
   const authInfo = await descopeClient.validateSession(sessionToken);
   console.log("Successfully validated user session:");
   console.log(authInfo.token.nsec.email);
